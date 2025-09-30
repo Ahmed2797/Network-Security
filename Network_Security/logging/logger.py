e4947caa63bfc0896193a0d3943d50fb151f8848
Ahmed2797 <tanvirahmed754575@gmail.com>
2025-09-30 19:12:17 +0600

    logging and exception

---
+++ Network_Security/logging/logger.py
@@ -0,0 +1,17 @@
+import os 
+import logging 
+from datetime import datetime 
+
+log_file = f'{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log'
+log_path = os.path.join(os.getcwd(),'logs',log_file)
+os.makedirs(log_path,exist_ok=True)
+
+log_file_path = os.path.join(log_path,log_file)
+
+logging.basicConfig(
+    filename= log_file_path,
+    level = logging.INFO,
+    format = '[%(asctime)s] %(lineno)s %(name)s - %(levelname)s - %(message)s'
+)
+
+logging.info("Logging setup completed successfully!")
